b2fc44d5354c1e2a8472dccf25eb86dc8b58a9ce
jhofmann794 <justinhofmann@pcp094011pcs.mu.edu>
2015-02-26 16:37:38 -0600

    Lists
    Added lists to Grocery and Inventory page

--- pocketFridge/app/src/main/java/pocketfridge/pocketfridge/groceryActivity.java
+++ pocketFridge/app/src/main/java/pocketfridge/pocketfridge/groceryActivity.java
@@ -1,12 +1,20 @@
 package pocketfridge.pocketfridge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class groceryActivity extends Activity {
@@ -15,6 +23,33 @@ public class groceryActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grocery);
+
+        final ListView listview = (ListView) findViewById(R.id.groceryList2);
+
+        String[] inventory = new String[] { "Ice Cream", "Chicken Patties", "Ravioli", "Orange Chicken",
+                "Tortellini", "Chicken Nuggets", "Apples", "Ho Hos", "Froot Loops", "Cheese",
+                "Hot Buns","Watermelon", "Kool Aid", "Noodles", "Butter", "Bread Crumbs",
+                "Cheese Curds", "Cool WHip"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < inventory.length; ++i) {
+            list.add(inventory[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview.setAdapter(adapter);
+
+        listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+
+            }
+
+        });
     }
 
 
@@ -49,5 +84,30 @@ public class groceryActivity extends Activity {
                 startActivity(new Intent(groceryActivity.this, MainActivity.class));
             }
         });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
