7488c1a22e589f7a6a115739e206082a7d1adfa1
jhofmann794 <justinhofmann@pcp085733pcs.mu.edu>
2015-03-03 16:38:32 -0600

    Design Updates
    Created a uniform theme to the application along with adding a checkout
    button to the Grocery List and added icons for the home screen.

--- pocketFridge/app/src/main/java/pocketfridge/pocketfridge/recipeActivity.java
+++ pocketFridge/app/src/main/java/pocketfridge/pocketfridge/recipeActivity.java
@@ -1,12 +1,20 @@
 package pocketfridge.pocketfridge;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 
 public class recipeActivity extends Activity {
@@ -15,6 +23,30 @@ public class recipeActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        final ListView listview2 = (ListView) findViewById(R.id.recipeList);
+
+        String[] inventory = new String[] { "Steak", "Omelette", "Brownies", "Chocolate Chip Cookies", "Tacos", "Pizza"};
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < inventory.length; ++i) {
+            list.add(inventory[i]);
+        }
+
+        final StableArrayAdapter adapter = new StableArrayAdapter(this,
+                android.R.layout.simple_list_item_1, list);
+        listview2.setAdapter(adapter);
+
+        listview2.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, final View view,
+                                    int position, long id) {
+                final String item = (String) parent.getItemAtPosition(position);
+
+            }
+
+        });
     }
 
 
@@ -49,5 +81,30 @@ public class recipeActivity extends Activity {
                 startActivity(new Intent(recipeActivity.this, MainActivity.class));
             }
         });
+    }
+
+    private class StableArrayAdapter extends ArrayAdapter<String> {
+
+        HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+        public StableArrayAdapter(Context context, int textViewResourceId,
+                                  List<String> objects) {
+            super(context, textViewResourceId, objects);
+            for (int i = 0; i < objects.size(); ++i) {
+                mIdMap.put(objects.get(i), i);
+            }
+        }
+
+        @Override
+        public long getItemId(int position) {
+            String item = getItem(position);
+            return mIdMap.get(item);
+        }
+
+        @Override
+        public boolean hasStableIds() {
+            return true;
+        }
+
     }
 }
